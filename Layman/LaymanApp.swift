4284d4efe7a9898ad89970cde5b6a6b14935d361
Kartik Gaur <horredaadi@gmail.com>
2026-04-05 11:28:40 +0530

    Initial Commit

---
+++ Layman/LaymanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaymanApp.swift
+//  Layman
+//
+//  Created by Kartik Gaur on 05/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LaymanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
